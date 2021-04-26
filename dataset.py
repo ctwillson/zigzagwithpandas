4a8467a95c78cdb47dc7c5f332648462f5bdc80b
ctwillson <anti.c@foxmail.com>
2021-04-26 23:52:41 +0800

    bug fix for label

--- dataset.py
+++ dataset.py
@@ -24,12 +24,18 @@ class MyData(Dataset):
 
 def collate_fn(data):
     # data.squesze(1)
+    traindata = data
+    data = []
+    label = []
+    for (x,y) in traindata:
+        data.append(x)
+        label.append(y)
     data.sort(key=lambda x: len(x), reverse=True)
     seq_len = [s.size(0) for s in data]
     data = pad_sequence(data, batch_first=True).float()    
     data = data.unsqueeze(-1)
     data = pack_padded_sequence(data, seq_len, batch_first=True)
-    return data
+    return data,label
 
 a = torch.tensor([1,2,3,4])
 b = torch.tensor([5,6,7])
@@ -48,7 +54,7 @@ for root, dirs, files in os.walk(true_file):
         df.drop('pre_close',axis=1,inplace=True)
         # df = df.apply(lambda x : (x-min(x)) / (max(x) - min(x)))
         X.append(torch.from_numpy(np.array(df.values, dtype=np.float32)))
-        Y.append(torch.from_numpy(np.array([1], dtype=np.float32)))
+        Y.append(torch.from_numpy(np.array(1, dtype=np.float32)))
         print(df.values)
     # assert 0
 print(X)
@@ -59,8 +65,8 @@ data = MyData(train_x,train_y)
 test = data[0]
 data_loader = DataLoader(data, batch_size=2, shuffle=True, collate_fn=collate_fn)
 batch_x = iter(data_loader).next()
-print(batch_x)
-# rnn = nn.LSTM(1, 4, 1, batch_first=True)
-# h0 = torch.rand(1, 2, 4).float()
-# c0 = torch.rand(1, 2, 4).float()
-# out, (h1, c1) = rnn(batch_x, (h0, c0))
+(a,b)=batch_x
+rnn = nn.LSTM(1, 4, 1, batch_first=True)
+h0 = torch.rand(1, 2, 4).float()
+c0 = torch.rand(1, 2, 4).float()
+out, (h1, c1) = rnn(a, (h0, c0))
