7e9ff975726b32499ee238e40b23189e034684cb
ctwillson <anti.c@foxmail.com>
2021-04-27 23:24:17 +0800

    bug fix

--- dataset.py
+++ dataset.py
@@ -33,7 +33,7 @@ def collate_fn(data):
     data.sort(key=lambda x: len(x), reverse=True)
     seq_len = [s.size(0) for s in data]
     data = pad_sequence(data, batch_first=True).float()    
-    data = data.unsqueeze(-1)
+    # data = data.unsqueeze(-1)
     data = pack_padded_sequence(data, seq_len, batch_first=True)
     return data,label
 
@@ -66,7 +66,9 @@ test = data[0]
 data_loader = DataLoader(data, batch_size=2, shuffle=True, collate_fn=collate_fn)
 batch_x = iter(data_loader).next()
 (a,b)=batch_x
-rnn = nn.LSTM(1, 4, 1, batch_first=True)
+rnn = nn.LSTM(7, 4, 1, batch_first=True)
+# h0 (num_layer,batchsize,hidden_size),和 input_size无关
+# offical doc :(num_layers * num_directions, batch, hidden_size)
 h0 = torch.rand(1, 2, 4).float()
 c0 = torch.rand(1, 2, 4).float()
 out, (h1, c1) = rnn(a, (h0, c0))
